f630a6ced3917f46c4295e8bd381ce2e73ac6d73
dmitridrozdov <dm.drozdov@gmail.com>
2022-11-21 16:18:39 +1100

    Update shazamCore.js

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -6,9 +6,13 @@ const options = {
       'X-RapidAPI-Key': 'df6dbe4c2emsh96b91ea5b45846cp18dcbfjsn3f3462d59ca3',
       'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
     }
-  };
+  }
   
-  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
     .then(response => response.json())
     .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .catch(err => console.error(err))
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+})
